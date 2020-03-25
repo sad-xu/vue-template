b40a64744c6fcdb9db71e16637bba21b1e2df214
sad-xu <1031568754@qq.com>
2020-03-25 18:06:32 +0800

    add: ez-table

--- src/views/example/table/Table.vue
+++ src/views/example/table/Table.vue
@@ -1,7 +1,103 @@
 <template>
   <div class="wrapper">
-    <h1>Example-table v0.2</h1>
-    <ez-table class="table-wrapper"></ez-table>
+    <h1>Ez-Table v0.3</h1>
+    <div class="desc">
+      <p class="title">
+        Table Attributes
+      </p>
+      <table>
+        <tr>
+          <th>参数</th>
+          <th>说明</th>
+          <th>类型</th>
+        </tr>
+        <tr>
+          <td>table-header-data</td>
+          <td>表头标题数据</td>
+          <td>[{ label, prop: String, width, fixed, sortable }]</td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td>label 标题 String</td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td>prop 属性名 String</td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td>width 列宽 String</td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td>fixed 是否固定在左侧 String</td>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td>sortable 是否排序 Boolean</td>
+        </tr>
+        <tr>
+          <td>table-body-data</td>
+          <td>主体数据</td>
+          <td>
+            [{ props: value }]
+          </td>
+        </tr>
+      </table>
+      <p class="title">
+        Table Events
+      </p>
+      <table>
+        <tr>
+          <th>事件名</th>
+          <th>说明</th>
+          <th>参数</th>
+        </tr>
+        <tr>
+          <td>@sortChange</td>
+          <td>排序事件</td>
+          <td>{ prop, order }</td>
+        </tr>
+        <tr>
+          <td>@rowClick</td>
+          <td>行点击事件</td>
+          <td>row, index</td>
+        </tr>
+      </table>
+      <p class="title">
+        Table Slot
+      </p>
+      <table>
+        <tr>
+          <th>用法</th>
+          <th>说明</th>
+        </tr>
+        <tr>
+          <td>v-slot:prop="{ row }"</td>
+          <td>自定义单元格内容</td>
+        </tr>
+      </table>
+    </div>
+    <ez-table
+      :table-header-data="tableHeaderData" :table-body-data="tableBodyData"
+      class="table-wrapper"
+      @sortChange="handleSortChange"
+      @rowClick="handleRowClick">
+      <template v-slot:prop-1="{ row }">
+        <span style="font-weight: bold;">{{ row['prop-1'] }}</span>
+      </template>
+      <template v-slot:prop-3="{ row }">
+        <el-button>{{ row['prop-3'] }}</el-button>
+      </template>
+      <template v-slot:prop-4="{ row }">
+        <span style="color:red;">{{ row['prop-4'] }}</span>
+      </template>
+    </ez-table>
   </div>
 </template>
 
@@ -15,23 +111,37 @@ export default {
   },
   data() {
     return {
-      tableData: [{
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-04',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1517 弄'
-      }, {
-        date: '2016-05-01',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1519 弄'
-      }, {
-        date: '2016-05-03',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1516 弄'
-      }]
+      tableHeaderData: Array.from({ length: 40 }).map((item, i) => ({
+        label: `index-${i}`,
+        prop: `prop-${i}`,
+        width: 100,
+        // 是否固定
+        fixed: i <= 1,
+        // 是否可排序
+        sortable: Boolean(Math.random() > 0.5)
+        // 内容自定义
+      })),
+      tableBodyData: Array.from({ length: 50 }).map((d, index) => {
+        let obj = {}
+        for (let i = 0; i < 40; i++) {
+          obj[`prop-${i}`] = `item-${i}-${index}`
+        }
+        return obj
+      })
+    }
+  },
+  methods: {
+    // 外部手动排序
+    handleSortChange({ order, prop }) {
+      if (order === 'ascending') {
+        this.tableBodyData.sort(() => Math.random() - 0.5)
+      } else if (order === 'descending') {
+        this.tableBodyData.sort(() => Math.random() - 0.5)
+      }
+    },
+    // 行点击事件
+    handleRowClick(row, index) {
+      console.log(row, index)
     }
   }
 }
@@ -39,13 +149,35 @@ export default {
 
 <style lang="scss" scoped>
   .wrapper {
-    height: 100%;
     background-color: #eee;
+    padding-bottom: 80px;
+  }
+
+  h1 {
+    padding: 2% 0 0 2%;
+  }
+  .desc {
+    display: flex;
+    flex-direction: column;
+    padding: 1% 2% 2% 2%;
+    .title {
+      font-family: -webkit-pictograph;
+      color: grey;
+    }
+    table {
+      font-size: 14px;
+      border-bottom: 1px solid #9e9e9e;
+      margin-bottom: 5px;
+      padding-bottom: 5px;
+      th {
+        text-align: left;
+      }
+    }
   }
 
   .table-wrapper {
-    width: 800px;
-    height: 600px;
-    margin: 0 auto;
+    width: 80%;
+    margin-left: 10%;
+    height: 500px;
   }
 </style>
--- src/views/example/table/ez-table.vue
+++ src/views/example/table/ez-table.vue
@@ -1,50 +1,81 @@
 <template>
-  <div class="ez-table">
-    <!-- <div class="ez-corner">
+  <div class="table-wrapper">
+    <!-- 左上角 -->
+    <div v-if="tableData.fixedHeader.length" class="ez-corner">
       <div
-        v-for="item in tableData.fixedHead" :key="item.prop"
-        :style="`width:${item.width}px;`" class="cell">
+        v-for="item in tableData.fixedHeader" :key="item.prop"
+        :style="`width:${item.width}px;`" class="cell"
+        @click="handleHeaderClick(item)">
         {{ item.label }}
-      </div>
-    </div> -->
-    <!-- 左侧固定列 -->
-    <div class="ez-side">
-      <div class="ez-side-head">
+        <!-- sort -->
         <div
-          v-for="item in tableData.fixedHead" :key="item.prop"
-          :style="`width:${item.width}px;`" class="cell">
-          {{ item.label }}
-        </div>
-      </div>
-      <div v-for="(row, rowIndex) in tableBodyData" :key="rowIndex" class="ez-side-row">
-        <div
-          v-for="col in tableData.fixedHead" :key="col.prop"
-          class="cell" :style="`width:${col.width}px;`">
-          {{ row[col.prop] }}
+          v-if="item.sortable" class="sort-wrapper"
+          :class="sortInfo.prop === item.prop ? `${sortInfo.order}-active` : ''">
+          <i class="ascending"></i>
+          <i class="descending"></i>
         </div>
       </div>
     </div>
     <!--  -->
-    <div class="ez-content" :style="`left: ${tableData.fixedWidth}px;`">
-      <div class="ez-content-head">
+    <div class="ez-table">
+      <!-- 左侧固定列 -->
+      <div v-if="tableData.fixedHeader.length" class="ez-side">
+        <!-- 被左上角覆盖 -->
+        <div class="ez-side-header" style="visibility: hidden;">
+          <div
+            v-for="item in tableData.fixedHeader" :key="item.prop"
+            :style="`width:${item.width}px;`" class="cell">
+            {{ item.label }}
+          </div>
+        </div>
         <div
-          v-for="item in tableData.normalHead" :key="item.label"
-          :style="`width:${item.width}px;`" class="cell">
-          {{ item.label }}
+          v-for="(row, rowIndex) in tableBodyData" :key="rowIndex"
+          class="ez-side-row" @click="handleRowClick(row, rowIndex)">
+          <div
+            v-for="col in tableData.fixedHeader" :key="col.prop"
+            class="cell" :style="`width:${col.width}px;`">
+            <!-- slot -->
+            <slot v-if="$scopedSlots[col.prop]" :row="row" :name="col.prop"></slot>
+            <span v-else>{{ row[col.prop] }}</span>
+          </div>
         </div>
       </div>
-      <table class="ez-content-table">
-        <colgroup>
-          <col v-for="item in tableData.normalHead" :key="item.prop" :width="item.width">
-        </colgroup>
-        <tr v-for="(row, rowIndex) in tableBodyData" :key="rowIndex">
-          <td v-for="(col, colIndex) in tableData.normalHead" :key="colIndex">
-            <div class="cell">
-              {{ row[col.prop] }}
+      <!-- 非固定列 -->
+      <div class="ez-content" :style="`left: ${tableData.fixedWidth}px;`">
+        <!-- header -->
+        <div class="ez-content-header">
+          <div
+            v-for="item in tableData.normalHeader" :key="item.prop"
+            :style="`width:${item.width}px;`" class="cell"
+            @click="handleHeaderClick(item)">
+            {{ item.label }}
+            <!-- sort -->
+            <div
+              v-if="item.sortable" class="sort-wrapper"
+              :class="sortInfo.prop === item.prop ? `${sortInfo.order}-active` : ''">
+              <i class="ascending"></i>
+              <i class="descending"></i>
             </div>
-          </td>
-        </tr>
-      </table>
+          </div>
+        </div>
+        <!-- content -->
+        <table class="ez-content-table">
+          <colgroup>
+            <col v-for="item in tableData.normalHeader" :key="item.prop" :width="item.width">
+          </colgroup>
+          <tr
+            v-for="(row, rowIndex) in tableBodyData" :key="rowIndex"
+            @click="handleRowClick(row, rowIndex)">
+            <td v-for="(col, colIndex) in tableData.normalHeader" :key="colIndex">
+              <!-- slot -->
+              <div class="cell">
+                <slot v-if="$scopedSlots[col.prop]" :row="row" :name="col.prop"></slot>
+                <span v-else>{{ row[col.prop] }}</span>
+              </div>
+            </td>
+          </tr>
+        </table>
+      </div>
     </div>
   </div>
 </template>
@@ -52,44 +83,64 @@
 <script>
 /*
 TODO
-  1. 排序
-  2. 函数式 solt
+  1. 排序 ✌
+  2. slot ✌
 
+  @sortChange 排序事件
+  @rowClick   行点击事件
 */
 
 export default {
   name: 'EzTable',
+  props: {
+    /**
+     * [{
+     *  label: String,
+     *  prop: String,
+     *  width: Number,
+     *  fixed: Boolean,
+     *  sortable: Boolean
+     * }]
+     *
+     */
+    tableHeaderData: {
+      type: Array,
+      required: true
+    },
+    /**
+     * [{
+     *  props: value
+     * }]
+     *
+     */
+    tableBodyData: {
+      type: Array,
+      required: true
+    }
+  },
   data() {
     return {
-      tableHeadData: Array.from({ length: 40 }).map((item, i) => ({
-        label: `index-${i}`,
-        prop: `prop-${i}`,
-        width: 100,
-        fixed: i <= 1
-      })),
-      tableBodyData: Array.from({ length: 50 }).map((d, index) => {
-        let obj = {}
-        for (let i = 0; i < 40; i++) {
-          obj[`prop-${i}`] = `item-${i}-${index}`
-        }
-        return obj
-      })
+      // 排序信息
+      sortInfo: {
+        prop: '',
+        order: '' // ascending, descending
+      }
     }
   },
   computed: {
     tableData: function() {
-      let fixedHead = []
-      let normalHead = []
+      let fixedHeader = []
+      let normalHeader = []
       let fixedWidth = 0
-      this.tableHeadData.forEach(item => {
+      this.tableHeaderData.forEach(item => {
         if (item.fixed) {
-          fixedHead.push(item)
+          fixedHeader.push(item)
           fixedWidth += item.width
-        } else normalHead.push(item)
+        } else normalHeader.push(item)
       })
       return {
-        fixedHead,
-        normalHead,
+        fixedHeader,
+        normalHeader,
         fixedWidth
       }
     }
@@ -98,45 +149,77 @@ export default {
     this.$nextTick(() => {
       let trList = this.$el.querySelector('.ez-content-table').querySelectorAll('tr')
       let sideRowList = this.$el.querySelectorAll('.ez-side-row')
+      // 同步固定列单元格高度
       sideRowList.forEach((row, index) => {
         row.style.height = trList[index].clientHeight + 'px'
       })
+      // 同步固定列表头高度
+      let headerHeight = this.$el.querySelector('.ez-content-header').clientHeight
+      this.$el.querySelector('.ez-side .ez-side-header').style.height = headerHeight + 'px'
+      this.$el.querySelector('.ez-corner').style.height = headerHeight + 'px'
     })
   },
   methods: {
-
+    // 表头点击事件
+    handleHeaderClick(item) {
+      if (item.sortable) {
+        if (item.prop === this.sortInfo.prop) {
+          this.sortInfo.order = this.sortInfo.order === 'ascending' ? 'descending' : 'ascending'
+        } else {
+          this.sortInfo = {
+            prop: item.prop,
+            order: 'ascending'
+          }
+        }
+        this.$emit('sortChange', this.sortInfo)
+      }
+    },
+    // 某一行点击事件
+    handleRowClick(row, rowIndex) {
+      this.$emit('rowClick', row, rowIndex)
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+  .table-wrapper {
+    position: relative;
+    overflow: hidden;
+    font-size: 14px;
+  }
+
+  // 单元格
+  .cell {
+    display: flex;
+    justify-content: left;
+    align-items: center;
+    flex-shrink: 0;
+    line-height: 23px;
+    min-height: 46px;
+    padding: 3px 10px;
+  }
+
+  // 左上角
+  .ez-corner {
+    display: flex;
+    flex-shrink: 0;
+    position: absolute;
+    left: 0;
+    top: 0;
+    background-color: #d6edff;
+    z-index: 3;
+    .cell {
+      min-height: 40px;
+    }
+  }
+
   .ez-table {
     position: relative;
-    font-size: 14px;
     overflow: scroll;
     will-change: scroll-position;
-
-    .ez-corner {
-      display: inline-flex;
-      position: sticky;
-      left: 0;
-      top: 0;
-      background-color: red;
-      z-index: 99;
-      .cell {
-          min-height: 40px;
-        }
-    }
-
-    .cell {
-      display: flex;
-      justify-content: left;
-      align-items: center;
-      flex-shrink: 0;
-      line-height: 23px;
-      min-height: 46px;
-      padding: 3px 10px;
-    }
+    height: 100%;
+    // 左侧固定列
     .ez-side {
       display: flex;
       flex-direction: column;
@@ -144,12 +227,12 @@ export default {
       float: left;
       left: 0;
       box-shadow: 0 0 10px rgba(0,0,0,.12);
-      z-index: 3;
-      .ez-side-head {
+      z-index: 2;
+      .ez-side-header {
         display: flex;
         flex-shrink: 0;
-        position: sticky;
-        top: 0;
+        // position: sticky;
+        // top: 0;
         background-color: #d6edff;
         .cell {
           min-height: 40px;
@@ -160,16 +243,17 @@ export default {
         flex-shrink: 0;
         align-items: center;
         background-color: #fff;
-        &:nth-of-type(even) {
+        &:nth-of-type(odd) {
           background-color: #edf5fe;
         }
       }
     }
+    // 右侧普通区
     .ez-content {
       position: absolute;
       display: flex;
       flex-direction: column;
-      .ez-content-head {
+      .ez-content-header {
         display: flex;
         flex-shrink: 0;
         position: sticky;
@@ -190,5 +274,33 @@ export default {
         }
       }
     }
+  }
+
+  .sort-wrapper {
+    position: relative;
+    height: 34px;
+    width: 24px;
+    .ascending, .descending {
+      position: absolute;
+      left: 7px;
+      width: 0;
+      height: 0;
+      border: 5px solid transparent;
+      transition: border-color 0.3s;
+    }
+    .ascending {
+      top: 5px;
+      border-bottom-color: #c0c4cc;
+    }
+    .descending {
+      bottom: 7px;
+      border-top-color: #c0c4cc;
+    }
+  }
+  .ascending-active .ascending {
+    border-bottom-color: #409eff;
+  }
+  .descending-active .descending {
+    border-top-color: #409eff;
   }
 </style>
