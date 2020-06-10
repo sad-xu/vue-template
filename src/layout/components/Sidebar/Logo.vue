9403f2f6724fb22a6b6808bfa7db82a0ef61928d
sad-xu <1031568754@qq.com>
2020-06-10 18:27:12 +0800

    update: logo css

--- src/layout/components/Sidebar/Logo.vue
+++ src/layout/components/Sidebar/Logo.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="sidebar-logo-container" :class="{'collapse':collapse}">
-    <transition name="sidebarLogoFade">
-      <router-link v-if="collapse" key="collapse" class="sidebar-logo-link" to="/">
-        <img v-if="logo" :src="logo" class="sidebar-logo">
-        <h1 v-else class="sidebar-title">
-          {{ title }}
-        </h1>
-      </router-link>
-      <router-link v-else key="expand" class="sidebar-logo-link" to="/">
-        <img v-if="logo" :src="logo" class="sidebar-logo">
-        <h1 class="sidebar-title">
-          {{ title }}
-        </h1>
-      </router-link>
-    </transition>
+    <!-- <transition name="sidebarLogoFade"> -->
+    <router-link key="collapse" class="sidebar-logo-link" to="/">
+      <!-- <img v-if="logo" :src="logo" class="sidebar-logo"> -->
+      <h1 class="sidebar-title">
+        {{ title }}
+      </h1>
+    </router-link>
+    <!-- <router-link v-else key="expand" class="sidebar-logo-link" to="/">
+      <img v-if="logo" :src="logo" class="sidebar-logo">
+      <h1 class="sidebar-title">
+        {{ title }}
+      </h1>
+    </router-link> -->
+    <!-- </transition> -->
   </div>
 </template>
 
@@ -50,37 +50,48 @@ export default {
   width: 100%;
   height: 50px;
   line-height: 50px;
-  background: #607d8b;
   text-align: center;
   overflow: hidden;
-
-  & .sidebar-logo-link {
+  border-bottom: 1px solid #eee;
+  .sidebar-logo-link {
     height: 100%;
     width: 100%;
-
-    & .sidebar-logo {
-      width: 32px;
-      height: 32px;
-      vertical-align: middle;
-      margin-right: 12px;
-    }
-
-    & .sidebar-title {
+    // .sidebar-logo {
+    //   width: 32px;
+    //   height: 32px;
+    //   vertical-align: middle;
+    //   margin-right: 12px;
+    // }
+    .sidebar-title {
+      font-size: 28px;
+      font-weight: bold;
       display: inline-block;
-      margin: 0;
-      color: #fff;
-      font-weight: 600;
-      line-height: 50px;
-      font-size: 14px;
-      font-family: Avenir, Helvetica Neue, Arial, Helvetica, sans-serif;
-      vertical-align: middle;
+      font-family: -webkit-pictograph, sans-serif;
+      background: linear-gradient(#36d1dc, #5b86e5);
+      background-clip: text;
+      -webkit-background-clip: text;
+      -webkit-text-fill-color: transparent;
+      transition: transform 0.3s;
+      animation: logoBg 3s linear infinite reverse;
     }
   }
 
   &.collapse {
-    .sidebar-logo {
-      margin-right: 0;
+    // .sidebar-logo {
+    //   margin-right: 0;
+    // }
+    .sidebar-title {
+      transform: scale(0.6) rotate(-30deg) translate(-25%, -25%);
     }
+  }
+}
+
+@keyframes logoBg {
+  0% {
+    filter: hue-rotate(0);
+  }
+  100% {
+    filter: hue-rotate(360deg);
   }
 }
 </style>
